9b052ca4c030520653db61d73da930797b341f22
kwpkielce <48807773+kwpkielce@users.noreply.github.com>
2020-03-08 10:54:16 +0100

    Add files via upload

---
+++ kalkulacja/result.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<title>Calculation</title>
+</head>
+	<body>
+	<?php
+		$num1 = $_POST['num1'];
+		$num2 = $_POST['num2'];
+		$a = $num1 + $num2;
+		
+		echo "The sum of ".$num1. " & " .$num2. " is "  . $a;
+		
+	?>
+	</body>
+</html>
